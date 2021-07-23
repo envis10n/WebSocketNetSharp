ac7b4033d78633e238cf5c950c4792168851fb72
envis10n <envis10n@envis10n.dev>
2021-07-23 15:10:22 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebSocketNetSharp
+
+A helpful wrapper around WatsonWebsocket.
---
+++ WebSocketNetSharp/WebSocketServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.WebSockets;
+using WatsonWebsocket;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WebSocketNetSharp
+{
+    public class WebSocketServerEvent<T>
+    {
+        public readonly T Event;
+    }
+    public class WebSocketServerBehavior
+    {
+
+    }
+    public class WebSocketServer
+    {
+    }
+}
